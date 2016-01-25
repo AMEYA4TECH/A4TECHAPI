ed950efe67d15d8f46330cde3d67b82b4e3f43e2
Amey <amey.more@a4technology.com>
2016-01-25 14:57:32 +0530

    Configured Spring.xml,Log4j,other propeties files & few login classes

---
+++ A4ASIConnect/src/com/a4/login/service/impl/LoginServiceImpl.java
@@ -0,0 +1,41 @@
+package com.a4.login.service.impl;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.client.RestTemplate;
+
+import com.a4.login.beans.AccessBean;
+import com.a4.login.beans.Login;
+import com.a4.loginservice.LoginService;
+
+public class LoginServiceImpl implements LoginService {
+
+	private Logger _LOGGER = Logger.getLogger(getClass());
+
+	AccessBean accessBean = null;
+	private RestTemplate restTemplateClass;
+
+	private String loginApiURL;
+
+	@Override
+	public AccessBean doLogin(Login login) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public RestTemplate getRestTemplateClass() {
+		return restTemplateClass;
+	}
+
+	public void setRestTemplateClass(RestTemplate restTemplateClass) {
+		this.restTemplateClass = restTemplateClass;
+	}
+
+	public String getLoginApiURL() {
+		return loginApiURL;
+	}
+
+	public void setLoginApiURL(String loginApiURL) {
+		this.loginApiURL = loginApiURL;
+	}
+
+}
---
+++ A4ASIConnect/src/com/a4/util/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package com.a4.util;
+
+public class ApplicationConstants {
+
+	private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SZ";
+	private final String CONST_MediaType = "application/json";
+	
+}
